6d3fbf239b4bc3a1605a6b4a9b9b7e86716fda18
mattslaney <mattslaney@users.noreply.github.com>
2024-04-13 19:03:53 +0100

    Update rust flake

--- develop/rust/flake.nix
+++ develop/rust/flake.nix
@@ -3,24 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }:
   let
     system = "x86_64-linux";
+    overlay-unstable = final: prev: {
+      unstable = import nixpkgs-unstable {
+        inherit system;
+      };
+    };
+    unstable-overlay = final: prev: {
+      unstable = import nixpkgs-unstable {
+        inherit system;
+      };
+    };
   in
   {
-    devShells."${system}".default = 
+    devShells."${system}".default =
     let
       pkgs = import nixpkgs {
-        inherit system;
+        inherit overlay-unstable system;
+        overlays = [ unstable-overlay ];
       };
     in pkgs.mkShell {
       packages = with pkgs; [
         cargo
-        rustc
+        unstable.rustc
         rustfmt
-        rust-analyzer
+        unstable.rust-analyzer
       ];
 
       shellHook = ''
@@ -38,3 +50,4 @@
     };
   };
 }
+
